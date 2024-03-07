1ee7b1b3cac9077e04bbb6ad02dc8b078c485b3d
owleek <owleek@MacBook-Pro-owleek.local>
2024-03-07 18:26:16 +0300

    guide draft

--- src/Root/root.jsx
+++ src/Root/root.jsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import Button from "../components/Button/Button";
 import {useEffect, useState} from "react";
-// import promo from '../assets/photo_2023-11-28 01.45.50.jpeg';
 const promo2 = require('../assets/photo_2023-11-28 01.45.50.jpeg');
 
 
 const Promo = () => {
-    useEffect(() => { document.cookie = "user=9" }, []);
     return <img style={{width: 'auto', height: '400px', maxWidth: '100%'}} src={promo2}/>
 }
 
 const Root = () => {
 
+    const [guideVisible, setGuideVisible] = useState(true);
     const cookies = document.cookie.split(';');
 
     const parsedCookies = cookies.map(str => ({
@@ -19,13 +18,71 @@ const Root = () => {
         val: str.split('=')[1]
     }));
 
-    const oldUser = parsedCookies.find(cookie => (cookie['key'] === 'user' && !!cookie['val']))
+    const oldUser = parsedCookies.find(cookie => (cookie['key'] === 'user' && !!cookie['val']));
 
+    const hideVideo = () => { document.cookie = "user=fer"; setGuideVisible(false) }
 
     return (
-        <div className="Root" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100%', width: '100%', backgroundImage: 'url(../assets/photo_2023-11-28 01.45.50.jpeg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-            { !oldUser && <Promo/> }
-            <Button style={{marginTop: '40px'}}>Приступить</Button>
+        <div className="Root" style={{height: '100%', width: '100%', backgroundImage: 'url(../assets/photo_2023-11-28 01.45.50.jpeg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+
+            {/* Guide */}
+            {
+                !oldUser && guideVisible && <div className="startPage" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100%', width: '100%', }}>
+                    <Promo/>
+                    <Button onClick={hideVideo} style={{marginTop: '40px'}}>Пропустить</Button>
+                </div>
+            }
+            {/* /Guide */}
+
+
+            {/*  Set & Go  */}
+            {
+                oldUser && !guideVisible &&
+                <div className={"Sets"} style={{
+                    width: '70vw',
+                    height: '70wh',
+                    alignSelf: 'center',
+                    border: '1px solid black',
+                    background: "white",
+                    padding: '2rem'
+                }}>
+                    <p>
+                        <p>Ultimates</p>
+                        <div style={{
+                            width: '100px',
+                            height: '100px',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            border: '1px solid brown'
+                        }}>+
+                        </div>
+                    </p>
+                    <p>
+                        <input type={'checkbox'}/>
+                        <span>Power Runes</span>
+                    </p>
+                    <p>
+                        <input type={'checkbox'}/>
+                        <span>Wisdom Runes</span>
+                    </p>
+                    <p>
+                        <input type={'checkbox'}/>
+                        <span>Healing lotus</span>
+                    </p>
+                    <p>
+                        <input type={'checkbox'}/>
+                        <span>Observers</span>
+                    </p>
+                    <p>
+                        <center>
+                            <button>Start game</button>
+                        </center>
+                    </p>
+                </div>
+            }
+
+
         </div>
     )
 }
