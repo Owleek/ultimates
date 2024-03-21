71ca2adbb7f811a303efc3fb5b8acbfaff20cdc2
owleek <owleek@MacBook-Pro-owleek.local>
2024-03-22 00:16:27 +0300

    styles optimized, font added, iframe size issue

--- src/components/Acquaintance/Acquaintance.jsx
+++ src/components/Acquaintance/Acquaintance.jsx
@@ -11,24 +11,23 @@ const Acquaintance: React.FC<IAcquaintance> = (props): React.JSX.Element => {
 
         <div className={'Acquaintance__Row'}>
             <div className={'Acquaintance__Column'}>
-                <div className={'MessageBox'}>
+                <p className={'Acquaintance__MessageBox'}>
                     Hello, I would like to introduce you to the Dota 2 assistant.
                     Once you get to know him, I think you'll want to continue using him.
                     I’ll tell you about all this in the video below, it’s quite short, only 5 minutes.
-                </div>
+                </p>
             </div>
             <div className={'Acquaintance__Column'}>
-                <div className={'proba'}>
+                <div className={'Acquaintance__Preview'}>
                     <div className={'VideoBox'}>
-                        <iframe className={'Video'} width="560" height="315" src="https://www.youtube.com/embed/Z9sug_QuhVc?si=QuGgyPySbn7onYmD"
+                        <iframe className={'Video'} src="https://www.youtube.com/embed/Z9sug_QuhVc?si=QuGgyPySbn7onYmD"
                                 title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowFullScreen></iframe>
                     </div>
                 </div>
             </div>
         </div>
-        <button className={'Acquaintance__button'}>Skip >></button>
     </div>
 }
 
