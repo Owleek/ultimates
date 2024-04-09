5e8b0b95c98b7fec56a61e9a44da938ebf917cb8
owleek <owleek@MacBook-Pro-owleek.local>
2024-04-09 21:29:00 +0300

    trying to desing the main playground

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import * as React from 'react';
 import Acquaintance from "./components/Acquaintance/Acquaintance";
 import TopPane from "./components/TopPane/TopPane";
+import Configuration from "./components/Configuration/Configuration";
 
 function App() {
     return <div className={'Root'}>
         <TopPane />
         <div className={'MainContainer'}>
-            <Acquaintance />
+            {/*<Acquaintance />*/}
+            {/*<Configuration />*/}
         </div>
     </div>
 }
--- src/components/Configuration/Configuration.jsx
+++ src/components/Configuration/Configuration.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Input from '../Input/Input';
 import './Configuration.scss';
+import '../Input/Input.scss';
+import Button from "../Button/Button";
 
 interface IConfiguration {
     props: any;
@@ -9,6 +10,17 @@ interface IConfiguration {
 const Configuration: React.FC<IConfiguration> = (props): React.JSX.Element => {
 
     return <div className={'Configuration'}>
+        <div className="Configuration__Frame">
+            <div className="Configuration__Header">
+                <h1 className="Configuration__Tittle">Configuration</h1>
+            </div>
+            <b/>
+            <b/>
+            <label>
+                <input type={'checkbox'} className={'checkbox'}/>
+            </label>
+            <Button className={'Configuration__Button'}>Start</Button>
+        </div>
         {/*<Input type="text" placeholder={'enter or scroll'}/>*/}
         {/*<Input className={'checkbox'} type={'checkbox'}/>*/}
         {/*<button className={'button'}>Start timer</button>*/}
